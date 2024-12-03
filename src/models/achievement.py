ed0d07606d5fa9954c027fcf4fcbe33a89055a1d
mistercapote <luizbravin0@gmail.com>
2024-12-03 15:00:16 -0300

    Achievements II

--- src/models/achievement.py
+++ src/models/achievement.py
@@ -1,13 +1,43 @@
+import pygame
+from constants import *
+pygame.init()
+def write(screen, text, font, color, center):
+    render = font.render(text, True, color)
+    rect = render.get_rect(center=center)
+    screen.blit(render, rect)
 
+#Tamanhos
+WIDTH_MAX = 1280
+HEIGHT_MAX = 720
+CENTER_X = WIDTH_MAX // 2
+CENTER_Y = HEIGHT_MAX // 2
+SQUARE_WIDTH = WIDTH_MAX // 20
+SQUARE_HEIGHT = HEIGHT_MAX // 12
+ACHIE_WIDTH = WIDTH_MAX // 5
+ACHIE_HEIGHT = HEIGHT_MAX // 3
+
+#Cores
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+
+FONT_GIGANT = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 90)
+FONT_BUTTON_HOVER = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 50)
+FONT_BUTTON = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 40)
+FONT_STORY = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 35)
+FONT_LARGE = pygame.font.Font("assets/font/Roboto_Slab/static/RobotoSlab-Regular.ttf", 20)
+FONT_SMALL= pygame.font.Font("assets/font/Roboto_Slab/static/RobotoSlab-Regular.ttf", 12)
 
 class Achievement:
-    def __init__(self, name, numbers, description, xpos, ypos):
+    def __init__(self, name, numbers, description, xpos, ypos, color):
         self.name = name
         self.numbers = numbers
-        self.done = False
+        self.done = True
         self.description = description
-        self.xpos = xpos * 1280 // 5
-        self.ypos = ypos * 720 // 3.5
+        self.xpos = (xpos - 0.5) * ACHIE_WIDTH
+        self.ypos = (ypos - 0.5)  * ACHIE_HEIGHT
+        self.color = color
     
     def family_list(self, game):
         conquista_atual = None
@@ -75,4 +105,17 @@ class Achievement:
             description = data["description"],
             xpos = data["xpos"],
             ypos = data["ypos"],
+            color = data["color"]
         )
+    
+    def draw(self, screen, coef=-1):
+        if not self.done:
+            name_text = ""
+            pygame.draw.rect(screen, GRAY, (self.xpos - coef, self.ypos - coef, ACHIE_WIDTH + 2*coef, ACHIE_HEIGHT + 2*coef))
+        else:
+            name_text = self.name
+            pygame.draw.rect(screen, self.color, (self.xpos - coef, self.ypos - coef, ACHIE_WIDTH + 2*coef, ACHIE_HEIGHT + 2*coef)) 
+        
+        write(screen, name_text, FONT_LARGE, BLACK, (self.xpos+ACHIE_WIDTH//2, self.ypos+ACHIE_HEIGHT//2))
+        
+    
--- src/views/achiev_menu.py
+++ src/views/achiev_menu.py
@@ -9,32 +9,32 @@ def achiev_menu(game):
         game.screen.fill(BLACK)
         xm, ym = pygame.mouse.get_pos()
         back_button.draw(game.screen)
-        write(game.screen, "Conquistas", FONT_BUTTON_HOVER, WHITE, (CENTER_X, CENTER_Y // 10))
+        write(game.screen, "Conquistas", FONT_BUTTON_HOVER, WHITE, (CENTER_X, CENTER_Y // 6))
         
         #Hover elements
-        hover_card = None
-        for card in ACHIEVEMENT:
-            if not hover_card:
-                if xm > card.xpos and xm < card.xpos + SQUARE_WIDTH and ym > card.ypos and ym < card.ypos + SQUARE_HEIGHT:
-                    hover_card = card
+        hover_achiev = None
+        for achiev in ACHIEVEMENT:
+            if not hover_achiev:
+                if xm > achiev.xpos and xm < achiev.xpos + ACHIE_WIDTH and ym > achiev.ypos and ym < achiev.ypos + ACHIE_HEIGHT:
+                    hover_achiev = achiev
                     continue
-            card.draw_card(game.screen)  
-        if hover_card: hover_card.draw_card(game.screen, -10)
+            achiev.draw(game.screen)  
+        if hover_achiev: hover_achiev.draw(game.screen, 20)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game.quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if game.table_popup: 
-                    game.table_popup[0].button.check_click(event, game, "table")
+                    game.table_popup[0].button.check_click(event, game, "achiev")
                 else:
-                    for card in table: 
-                        if isinstance(card.entity, Element):
-                            if event.pos[0] > card.xpos and event.pos[0] < card.xpos + SQUARE_WIDTH and event.pos[1] > card.ypos and event.pos[1] < card.ypos + SQUARE_HEIGHT:
-                                game.table_popup.append(card)
+                    for achiev in ACHIEVEMENT: 
+                        if achiev.done == True:
+                            if event.pos[0] > achiev.xpos and event.pos[0] < achiev.xpos + SQUARE_WIDTH and event.pos[1] > achiev.ypos and event.pos[1] < achiev.ypos + SQUARE_HEIGHT:
+                                game.table_popup.append(achiev)
                 running = back_button.check_click(event, running)
         
-        if game.table_popup: 
-            game.table_popup[0].draw_popup(game)
+        # if game.achiev_popup: 
+        #     game.table_popup[0].draw_popup(game)
          
         pygame.display.flip()
