72d80addb13f230a0815c90a32f2192ad9247721
mistercapote <luizleb70@gmail.com>
2024-12-03 16:50:54 -0300

    Fase 1

--- src/models/achievement.py
+++ src/models/achievement.py
@@ -1,5 +1,4 @@
 import pygame
-from constants import *
 from models.fusion import Element
 
 pygame.init()
@@ -7,29 +6,14 @@ def write(screen, text, font, color, center):
     render = font.render(text, True, color)
     rect = render.get_rect(center=center)
     screen.blit(render, rect)
-
-#Tamanhos
+    
 WIDTH_MAX = 1280
 HEIGHT_MAX = 720
-CENTER_X = WIDTH_MAX // 2
-CENTER_Y = HEIGHT_MAX // 2
-SQUARE_WIDTH = WIDTH_MAX // 20
-SQUARE_HEIGHT = HEIGHT_MAX // 12
 ACHIE_WIDTH = WIDTH_MAX // 5
 ACHIE_HEIGHT = HEIGHT_MAX // 3
-
-#Cores
-WHITE = (255, 255, 255)
 GRAY = (200, 200, 200)
 BLACK = (0, 0, 0)
-YELLOW = (255, 255, 0)
-
-FONT_GIGANT = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 90)
-FONT_BUTTON_HOVER = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 50)
-FONT_BUTTON = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 40)
-FONT_STORY = pygame.font.Font("assets/font/Bungee_Inline/BungeeInline-Regular.ttf", 35)
 FONT_LARGE = pygame.font.Font("assets/font/Roboto_Slab/static/RobotoSlab-Regular.ttf", 20)
-FONT_SMALL= pygame.font.Font("assets/font/Roboto_Slab/static/RobotoSlab-Regular.ttf", 12)
 
 class Achievement:
     def __init__(self, name, numbers, description, xpos, ypos, color):
