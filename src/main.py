be2f25fa04d455462c526d57a6dcbe5d40f6d62c
mistercapote <luizleb70@gmail.com>
2024-12-03 15:00:16 -0300

    Achievements II

--- src/main.py
+++ src/main.py
@@ -4,41 +4,46 @@ from models.button import OpeningButton
 from models.game import Game
 from views import achiev_menu, start_menu_1, start_menu_2, story_menu, table_menu
 
-#Inicialização
-game = Game()
-# game.update_for_level_2()
+def main():
+    #Inicialização
+    game = Game()
+    # game.update_for_level_2()
 
-#Definindo os botões da tela inicial
-start_button = OpeningButton(game.screen, "Start", CENTER_X, CENTER_Y - 70, start_menu_1.start_menu if game.current_phase == 1 else start_menu_2.start_menu)
-story_button = OpeningButton(game.screen, "Story", CENTER_X, CENTER_Y, story_menu.story_menu)
-table_button = OpeningButton(game.screen, "Periodic Table", CENTER_X, CENTER_Y + 70, table_menu.table_menu)
-settings_button = OpeningButton(game.screen, "Achievement", CENTER_X, CENTER_Y + 140, achiev_menu.achiev_menu)
-exit_button = OpeningButton(game.screen, "Exit", CENTER_X, CENTER_Y + 210, game.quit)
+    #Definindo os botões da tela inicial
+    start_button = OpeningButton(game.screen, "Start", CENTER_X, CENTER_Y - 70, start_menu_1.start_menu if game.current_phase == 1 else start_menu_2.start_menu)
+    story_button = OpeningButton(game.screen, "Story", CENTER_X, CENTER_Y, story_menu.story_menu)
+    table_button = OpeningButton(game.screen, "Periodic Table", CENTER_X, CENTER_Y + 70, table_menu.table_menu)
+    settings_button = OpeningButton(game.screen, "Achievement", CENTER_X, CENTER_Y + 140, achiev_menu.achiev_menu)
+    exit_button = OpeningButton(game.screen, "Exit", CENTER_X, CENTER_Y + 210, game.quit)
 
-#Loop principal
-running = True
-while running:
-    game.screen.fill(BLACK) #Limpar tela
-    game.updateVideoFrame() #Atualizar video de fundo
-    game.draw_title() #Escrever titulo
+    #Loop principal
+    running = True
+    while running:
+        game.screen.fill(BLACK) #Limpar tela
+        game.updateVideoFrame() #Atualizar video de fundo
+        game.draw_title() #Escrever titulo
 
-    #Para cada evento detectado
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-            game.quit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            #Ao clicar em cada botão
-            start_button.check_click(event, game)
-            story_button.check_click(event, game)
-            table_button.check_click(event, game)
-            settings_button.check_click(event, game)
-            exit_button.check_click(event, game)
+        #Para cada evento detectado
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                game.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #Ao clicar em cada botão
+                start_button.check_click(event, game)
+                story_button.check_click(event, game)
+                table_button.check_click(event, game)
+                settings_button.check_click(event, game)
+                exit_button.check_click(event, game)
 
-    #Hover effect
-    start_button.draw(game.screen)
-    story_button.draw(game.screen)
-    table_button.draw(game.screen)
-    settings_button.draw(game.screen)
-    exit_button.draw(game.screen)
+        #Hover effect
+        start_button.draw(game.screen)
+        story_button.draw(game.screen)
+        table_button.draw(game.screen)
+        settings_button.draw(game.screen)
+        exit_button.draw(game.screen)
 
-    pygame.display.flip()
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
